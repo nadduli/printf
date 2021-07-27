e38ac8f25dde38e2fb0696083a41123356d65c1f
nadduli <naddulidaniel94@gmail.com>
2021-07-27 06:10:30 -0700

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,144 +1,183 @@
 #include "holberton.h"
-#include <stdlib.h>
-
 /**
-<<<<<<< HEAD
- * validate_operator - validate if it is an operator
- * @format: Format to print to the console
- * Return: 1 if is positive; 0 if is negative
- */
-int validate_operator(char format)
-{
-	char operator[] = {'c', 's', 'd', 'i', 'u', 'x',
-						 'X', 'S', 'o', 'r', 'R', 'b', '%'};
-	int i = 0;
-
-	while (operator[i])
-	{
-		if (operator[i] == format)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-/**
- * _printf - Simulates the printf function
- * @format: Format to print to the console
- * Return: number of char printed
-=======
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
->>>>>>> 1f77af68a235e3ad4a9be6206d5b2d8ec911066d
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-<<<<<<< HEAD
-	unsigned int i = 0, result = 0;
-	va_list list;
-
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	while (format && format[i])
-	{
-		if (format[i] != '%')
-			result += _putchar(format[i]);
-		else
-		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-			while (format[i] == ' ')
-				i++;
-			if (format[i] == '%')
-				result += _putchar(format[i]);
-			else if (validate_operator(format[i]) == 0)
-			{
-				result += _putchar('%');
-				if (format[i - 1] == ' ')
-					result += _putchar(' ');
-				result += _putchar(format[i]);
-			}
-			else
-				result += select_function(format[i], list);
-		}
-		i++;
-	}
-	va_end(list);
-	return (result);
-=======
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
+ * _printf - a function that prints anything using
+ *           posix callex via the write function
+ * @format: formated string to be printed
+ * Return: the number of things printed
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	va_list items;
+	int i = 0, len = 0, *printed, *buffindex = malloc(sizeof(int));
+	char buffer[BUFFER_SIZE];
 
-	if (format == NULL)
+	if (!format || !buffindex)
 		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	if (*format == '%' && _strlen((char *)format) == 1)
+		return (-1);
+
+	*buffindex = 0;
+	va_start(items, format);
+	while (*(format + i))
 	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
+		printed = print(format + i, items, buffindex, buffer);
+		if (!printed)
 			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
+		len += printed[1];
+		i += printed[0];
+		free(printed);
 	}
-	va_end(valist);
-	return (count);
->>>>>>> 1f77af68a235e3ad4a9be6206d5b2d8ec911066d
+	if (*buffindex)
+	{
+		i = write(1, buffer, *buffindex);
+		if (i == -1)
+			return (-1);
+		len += i;
+	}
+	va_end(items);
+	free(buffindex);
+	return (len);
+}
+
+/**
+ *parse_format - takes a string and parse it in a printing format
+ *for example if the str starts with %34f, this will be parse as
+ *a float of length 34. This function always assums that the str
+ *passed starts with %
+ *@s: the string to be parsed that starts with %
+ *Return:a struct containg the parsed values
+ */
+printing_format *parse_format(const char *s)
+{
+	int i = 1;
+	char tmp;
+	printing_format *format = malloc(sizeof(printing_format));
+
+	if (!format)
+		return (NULL);
+	format->replaced = 0, format->width = 0, format->validity = false;
+
+	i += checkflag(format, *(s + i));
+	i += checkwidth(format, s + i);
+
+	/*make sure that no usless character in between exists*/
+	tmp = *(s + i);
+	if (tmp == '.' || is_valid_id(tmp) || tmp == 'l' || tmp == 'h')
+	{
+		if (*(s + i) == '.')
+		{
+			i++;
+			i += checkprecision(format, s + i);
+		}
+		i += checkmod(format, s + i);
+
+		if (is_valid_id(*(s + i)))
+		{
+			format->printer = get_printer(*(s + i));
+			format->replaced += 1;
+			i++;
+		}
+		else
+		{
+			format->validity = false;
+			return (format);
+		}
+		format->validity = true;
+		format->replaced += 1;
+	}
+	return (format);
+}
+/**
+ *buf_push - saves a string to a temporaty printing buffer
+ *and returns the length of things compied or -1 on failure
+ *@str:string top be copied
+ *@buffindex: the current index of the buffer
+ *@buffer: the printint que
+ *Return: the length of string printed (the once that overflowed)
+ *from the buffer
+ */
+int buf_push(char *str, int *buffindex, char *buffer)
+{
+	int tmplen = _strlen(str), k = 0, printed = 0;
+	char null[] = "(null)";
+
+	if (str == NULL)
+		return (buf_push(null, buffindex, buffer));
+	if (str == '\0')
+		return (printed);
+	while (tmplen)
+	{
+		while (*buffindex + 1 < BUFFER_SIZE && *str)
+		{
+
+			buffer[*buffindex] = *(str);
+			*buffindex += 1, str += 1, tmplen -= 1;
+		}
+		if (*buffindex + 1 >= BUFFER_SIZE)
+		{
+			k = write(1, buffer, BUFFER_SIZE);
+			if (k == -1)
+				return (-1);
+			*buffindex = 0;
+			printed += k;
+		}
+
+	}
+	return (printed);
+}
+
+/**
+ *print - stores an element into a buffer based on a formated string
+ *it also takes va_list incase the formated strin refers to an element
+ *  a printer function that is passed as an argment, if buffer is full
+ * prints and reinitializes the buffer
+ *@str:  format string
+ *@items: va_list containing the item to be printed accouring to the format
+ *@buffindex: the current index of the printe queue
+ *@buffer: printing queue
+ *Return: [replaced formate string, len of printed]
+ * replaced or [-1, -1] on error
+ */
+int *print(const char *str, va_list items, int *buffindex, char *buffer)
+{
+	char *tmp = NULL, perstr[] = "%", *tmpstr = malloc(sizeof(char) * 2);
+	int *buf_stat = malloc(sizeof(int) * 2);
+	printing_format *format = NULL;
+
+	if (!tmpstr || !buf_stat)
+		return (NULL);
+	tmpstr[1] = '\0', buf_stat[0] = buf_stat[1] = -1;
+	if (*(str) != '%')
+	{
+		tmpstr[0] = *str;
+		buf_stat[1] = buf_push(tmpstr, buffindex, buffer), buf_stat[0] = 1;
+	}
+	else
+	{
+		format = parse_format(str);
+		if (format)
+		{
+			if (format->validity)
+			{
+				tmp = (format->printer)(items, format);
+				if (!tmp)
+					tmp = NULL;
+				buf_stat[1] = buf_push(tmp, buffindex, buffer);
+				buf_stat[0] = format->replaced;
+				free(tmp);
+			}
+			else
+			{
+				buf_stat[1] = buf_push(perstr, buffindex, buffer);
+				/*incase I have %% the second one must be ignored*/
+				if (*(str + 1) == '%')
+					buf_stat[0] = 2;
+				else
+					buf_stat[0] = 1;
+			}
+		}
+	}
+	free(format);
+	free(tmpstr);
+	return (buf_stat);
 }
--- holberton.h
+++ holberton.h
@@ -1,85 +1,87 @@
-<<<<<<< HEAD
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef PRINTF
+#define PRINTF
 
+#define BIT_SIZE 8
+#define true 1
+#define false 0
+typedef unsigned int bool;
+/*
+ *   a local buffer which will be used
+ *   to store data until a syscall is made to write it
+ *   in the std out
+ */
+#define BUFFER_SIZE 1024
+
+#include <stdarg.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <unistd.h>
-#include <limits.h>
 
-#define _INT_MAX_ 2147483647
-/*fff*/
 /**
- * struct Ftype - Struct operator
- *
- * @c: Parameter char (%s, %c, %i, ...)
- * @f: The function associated
- */
-struct Ftype
+*struct printing_format - a struct for formating info
+*@flag: the flag used (+, -, '\0')
+*@width: the space taken during printing
+*@mod: modifier (l, h)
+*@precision: how manyt points after . in case of f
+*@zero_fill: --
+*@replaced: the total amount the format is holding
+*@validity: is this format a complete format
+*@printer: a function to handle the printing
+*/
+typedef struct printing_format
 {
-	char c;
-	int (*f)(va_list);
-} ftype;
-typedef struct Ftype f_type;
+	char flag;
+	int width;
+	char mod;
+	int precision;
+	bool zero_fill;
+	int replaced;
+	bool validity;
+	char *(*printer)(va_list, struct printing_format *);
+} printing_format;
+/*printer functions*/
+char *_putchar(va_list, printing_format *);
+char *_putstr(va_list, printing_format *);
+char *_putint(va_list, printing_format *);
+char *_putuint(va_list, printing_format *);
+char *_putbin(va_list, printing_format *);
+char *_puthex(va_list, printing_format *);
+char *_putoct(va_list, printing_format *);
+char *_putHex(va_list, printing_format *);
+char *_putadress(va_list, printing_format *);
+char *_putrts(va_list, printing_format *);
+char *_putrot13(va_list, printing_format *);
+char *_putS(va_list, printing_format *);
 
-/* main functions */
+/*printf and its helpers*/
 int _printf(const char *format, ...);
-int select_function(char format, va_list list);
+printing_format *parse_format(const char *);
+int buf_push(char *, int *, char *);
+int *print(const char *, va_list, int *, char *);
 
-/* print functions : type char */
-int print_char(va_list lista);
-int print_str(va_list lista);
-int print_rev_str(va_list lista);
-int print_Str_Ascii(va_list arg);
+/*validity checker*/
+bool is_valid_id(char);
+int checkflag(printing_format *, char);
+int checkwidth(printing_format *, const char *);
+int checkprecision(printing_format *, const char *);
+int checkmod(printing_format *, const char *);
 
-/* print functions : type numbers */
-int putN(unsigned int n, unsigned int b, char *nums);
-int print_int(va_list lista);
-int print_bin(va_list lista);
-int print_unsigned(va_list lista);
+/*parser to identify printing format*/
+printing_format *parse_format(const char *);
 
-/* print bases */
-int print_octal(va_list lista);
-int print_rot13(va_list lista);
-int print_hex(va_list lista);
-int print_HEX(va_list lista);
+/*printer identifier*/
+char *(*get_printer(char id))(va_list, printing_format *);
 
-/* functions from standar library in f_library like putchar and others*/
-int _putchar(char c);
-int _strlen(char *s);
-int _atoi(char *str);
-int	print_HEXADECIMAL(unsigned int n);
-=======
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+/*num_utils*/
+int _pow(unsigned int, int);
+int _numLen(unsigned int);
+int max(int, int);
+char *to_hex(int);
 
-#include <stdarg.h>
-/**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
+/*string utils*/
+int _strlen(char *);
+void rev_string(char *);
+void _toStr(unsigned long int, char *);
+char *_strcpy(char *, char *);
+char *rot13(char *);
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
->>>>>>> 1f77af68a235e3ad4a9be6206d5b2d8ec911066d
-
-#endif  /* _HOLBERTON_H */
+#endif /*PINTF*/
