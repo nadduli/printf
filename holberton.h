1f77af68a235e3ad4a9be6206d5b2d8ec911066d
Stacey-bee <staceynakanwagi@gmail.com>
2021-07-27 15:16:51 +0300

    modifying holberton.h

--- holberton.h
+++ holberton.h
@@ -1,30 +1,32 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
 /**
- * struct convert - defines a structure for symbols and functions
- * @sym: The operator
- * @f: function associated
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-struct convert
+typedef struct print
 {
-	char *sym;
+	char *t;
 	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+} print_t;
 
-/* Main functions */
-int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif
+#endif  /* _HOLBERTON_H */
